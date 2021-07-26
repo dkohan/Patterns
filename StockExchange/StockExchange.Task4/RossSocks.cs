a37e592f8df63a0854c18c941776a499badb28d7
MINSK\Dzmitriy_Kakhanouski <kohanowskij@gmail.com>
2021-07-26 20:04:07 +0300

    Code review

--- StockExchange/StockExchange.Task4/RossSocks.cs
+++ StockExchange/StockExchange.Task4/RossSocks.cs
@@ -4,9 +4,9 @@ namespace StockExchange.Task4
 {
     public class RossSocks
     {
-        public int SoldShares { get; }
+        public int SoldShares { get; private set; }
 
-        public int BoughtShares { get; }
+        public int BoughtShares { get; private set; }
 
         public RossSocks()
         {
@@ -14,12 +14,42 @@ namespace StockExchange.Task4
 
         public bool SellOffer(string stockName, int numberOfShares)
         {
-            throw new NotImplementedException();
+            StockMarket.StockRequestCompleted += Update;
+            return StockMarket.MakeStockRequest(new Stock(false, stockName, numberOfShares, nameof(RossSocks)));
         }
 
         public bool BuyOffer(string stockName, int numberOfShares)
         {
-            throw new NotImplementedException();
+            StockMarket.StockRequestCompleted += Update;
+            return StockMarket.MakeStockRequest(new Stock(true, stockName, numberOfShares, nameof(RossSocks)));
         }
+
+        void Update(object sender, Stock stock)
+        {
+            if (stock.Player == nameof(RossSocks))
+            {
+                if (stock.Type)
+                {
+                    BoughtShares += stock.Number;
+                }
+                else
+                {
+                    SoldShares += stock.Number;
+                }
+            }
+            else
+            {
+                if (!stock.Type)
+                {
+                    BoughtShares += stock.Number;
+                }
+                else
+                {
+                    SoldShares += stock.Number;
+                }
+            }
+        }
+
+
     }
 }
